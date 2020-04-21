e4bf8babd82a2c87c8ab70e34727d3067f1b51ba
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2020-04-21 23:38:53 +0100

    prod profile for aws deploy

--- start_app.sh
+++ start_app.sh
@@ -4,7 +4,7 @@ echo ""
 echo ""
 echo ""
 
-sudo nohup java -jar app/build/libs/user-location-api.jar &
+sudo nohup java -jar app/build/libs/user-location-api.jar -Dspring.profiles.active=prod &
 
 echo "        User location service launched successfully"
 echo ""
